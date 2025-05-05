700120a6ed6f7052c72f07aafb3ddcfc8a73decb
Janeček Pavel <L20220039@prumyslovkajihlava.cz>
2025-05-05 12:27:33 +0200

    niga

---
+++ my-vue-app/src/components/prevod.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export const Prevod = () => {
+    const [cislo, setCislo] = useState(0);
+
+    return(
+        <div>
+            <h1>
+                Převod EUR do jiné měny
+            </h1>
+            <div>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    style={{ padding: '5px' }}
+                />
+            </div>
+        </div>
+    )
+}
